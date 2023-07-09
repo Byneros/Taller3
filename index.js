fc1dc8e731b71b1cbdb3f6698a0a5263401d7c59
Byneros <ing.nelsonjulio@gmail.com>
2023-07-09 13:48:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.use(express.json());
 app.use(cors);
 conectarbd();
 app.use('/empleados', RutaEmpleado);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running  on port ${PORT}`));
 
